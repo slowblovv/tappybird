05c80096d23e86b263b50d7b0ed1357539a82a12
slowblovv <petrovbaydam06@gmail.com>
2026-02-20 11:25:55 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './index.css';
+(async function() {
+  try {
+    const { init, isTMA, viewport } = await import('@telegram-apps/sdk');
+
+    if (await isTMA()) {
+      init();
+      if (viewport.mount.isAvailable()) {
+        await viewport.mount();
+        await viewport.expand();
+      }
+      if (viewport.requestFullscreen.isAvailable()) {
+        await viewport.requestFullscreen();
+      }
+    }
+  } catch (e) {
+    console.warn('TG SDK error / not available', e);
+  }
+})();
