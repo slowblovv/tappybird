49dbc83d38572245852652bf26617ec0206b6f14
slowblovv <petrovbaydam06@gmail.com>
2026-02-20 11:23:35 +0900

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,37 @@
+// src/App.jsx
+import React, { useEffect } from 'react';
+import { init, isTMA, viewport } from '@telegram-apps/sdk';
+
+export default function App() {
+  useEffect(() => {
+    async function initTg() {
+      try {
+        if (await isTMA()) {
+          init();
+
+          if (viewport.mount.isAvailable()) {
+            await viewport.mount();
+            await viewport.expand();
+          }
+
+          if (viewport.requestFullscreen.isAvailable()) {
+            await viewport.requestFullscreen();
+          } else {
+            console.log('viewport.requestFullscreen not available');
+          }
+        } else {
+          console.log('Not running inside Telegram Mini App (isTMA false)');
+        }
+      } catch (err) {
+        console.error('Telegram fullscreen init error:', err);
+      }
+    }
+    initTg();
+  }, []);
+
+  return (
+    <div id="root">
+      {/* ваш интерфейс */}
+    </div>
+  );
+}
