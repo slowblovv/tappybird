06acf9afe66765cc67218b6b7926fc16b5750c13
slowblovv <petrovbaydam06@gmail.com>
2026-02-20 10:42:08 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './index.css';
+import { init, isTMA, viewport } from '@telegram-apps/sdk';
+
+async function start() {
+  if (await isTMA()) {
+    init();
+
+    if (viewport.mount.isAvailable()) {
+      await viewport.mount();
+      viewport.expand();
+    }
+
+    if (viewport.requestFullscreen.isAvailable()) {
+      await viewport.requestFullscreen();
+    }
+  }
+}
+
+// Запуск когда DOM готов
+if (document.readyState === 'complete' || document.readyState === 'interactive') {
+  start();
+} else {
+  window.addEventListener('DOMContentLoaded', start, { once: true });
+}
