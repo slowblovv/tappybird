76ba6c91a57fa75050819048f40db90882fdb0c0
slowblovv <petrovbaydam06@gmail.com>
2026-02-20 11:41:04 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import './index.css';
-(async function() {
-  try {
-    const { init, isTMA, viewport } = await import('@telegram-apps/sdk');
 
-    if (await isTMA()) {
-      init();
-      if (viewport.mount.isAvailable()) {
-        await viewport.mount();
-        await viewport.expand();
-      }
-      if (viewport.requestFullscreen.isAvailable()) {
-        await viewport.requestFullscreen();
+useEffect(() => {
+    async function initTg() {
+      if (await isTMA()) {
+        init();
+
+        if (viewport.mount.isAvailable()) {
+          await viewport.mount();
+          viewport.expand();
+        }
+
+        if (viewport.requestFullscreen.isAvailable()) {
+          await viewport.requestFullscreen();
+        }
       }
     }
-  } catch (e) {
-    console.warn('TG SDK error / not available', e);
-  }
-})();
+    initTg();
+
+  }, []);
